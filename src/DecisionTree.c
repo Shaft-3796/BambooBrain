355b41e63166d8c6c732489cf87249c041c11ed5
shaft <sh4ft.me@gmail.com>
2023-12-17 15:20:01 +0100

    refacto(dt): clean DecisionTree (dt)

--- src/DecisionTree.c
+++ src/DecisionTree.c
@@ -1,53 +1,49 @@
-//
-// Created by loe on 14/12/23.
-//
-
 #include "DecisionTree.h"
 
-int getMajClass(Subproblem* sp) {
-    int maj = -1;
-    int majID = -1;
-    for (int i = 0; i < sp->classCount; ++i) {
-        if (sp->classes[i].instanceCount > maj) {
-            maj = sp->classes[i].instanceCount;
-            majID = i;
+int get_main_class(Subproblem *sp) {
+    int mainId = 0;
+    for(int classId=1; classId<sp->classCount; ++classId) {
+        if(sp->classes[classId].instanceCount > sp->classes[classId-1].instanceCount) {
+            mainId = classId;
         }
     }
-    return majID;
+    return mainId;
 }
 
-float getPurity(Subproblem* sp, int majID) {
-    if (!sp->instanceCount) return 0.0;
-
-    return (float)sp->classes[majID].instanceCount / (float)sp->instanceCount;
+// If the subproblem is pure enough, we return the main class id, else we return -1
+int test_node_purity(Subproblem *sp, float prunningTreshold) {
+    const int mainId = get_main_class(sp);
+    return (float)sp->classes[mainId].instanceCount / (float)sp->instanceCount >= prunningTreshold ? mainId : -1;
 }
 
-DecisionTreeNode* DecisionTree_create(Subproblem* sp, int currentDepth, int maxDepth, float pruningThreshold) {
-    DecisionTreeNode* node = (DecisionTreeNode*) calloc(1, sizeof(DecisionTreeNode));
+/**
+ * @brief DecisionTree_create creates a decision tree from a subproblem
+ * @param sp the subproblem
+ * @param currentDepth the current depth of the tree
+ * @param maxDepth the maximum depth of the tree
+ * @param prunningThreshold the prunning treshold
+ * @return a pointer to the decision tree
+ */
+DecisionTreeNode* DecisionTree_create(Subproblem *sp, int currentDepth, int maxDepth, float prunningThreshold) {
+    DecisionTreeNode *node = (DecisionTreeNode*)calloc(1, sizeof(DecisionTreeNode));
 
-    int majID = getMajClass(sp);
-    if (getPurity(sp, majID) >= pruningThreshold || currentDepth >= maxDepth) {
-        node->classID = majID;
-        return node;
-    }
+    // Max depth reached
+    if(currentDepth >= maxDepth) { node->classID = get_main_class(sp); return node;}
+
+    // Subproblem is pure enough
+    int mainId = test_node_purity(sp, prunningThreshold);
+    if(mainId != -1) { node->classID = mainId; return node; }
+
+    // Split the subproblem
+    Subproblem *spl = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+    Subproblem *spr = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+
     node->split = Split_compute(sp);
 
-    Subproblem *spl = Subproblem_create(sp->capacity, sp->featureCount, sp->classCount);
-    Subproblem *spr = Subproblem_create(sp->capacity, sp->featureCount, sp->classCount);
+    Split_subproblem(sp, node->split.featureID, node->split.threshold, spl, spr);
 
-    // Split sp into 2 sub problems
-    for (int i = 0; i < sp->instanceCount; ++i) {
-        int value = sp->instances[i]->values[node->split.featureID];
-        if ((float)value <= node->split.threshold) {
-            Subproblem_insert(spl, sp->instances[i]);
-        }
-        else {
-            Subproblem_insert(spr, sp->instances[i]);
-        }
-    }
-
-    node->left = DecisionTree_create(spl, currentDepth+1, maxDepth, pruningThreshold);
-    node->right = DecisionTree_create(spr, currentDepth+1, maxDepth, pruningThreshold);
+    node->left = DecisionTree_create(spl, currentDepth+1, maxDepth, prunningThreshold);
+    node->right = DecisionTree_create(spr, currentDepth+1, maxDepth, prunningThreshold);
 
     Subproblem_destroy(spl);
     Subproblem_destroy(spr);
@@ -55,19 +51,25 @@ DecisionTreeNode* DecisionTree_create(Subproblem* sp, int currentDepth, int maxD
     return node;
 }
 
+/**
+ * @brief Decision_nodeCount counts the number of nodes in a decision tree
+ * @param node the node
+ * @return the number of nodes
+ */
 int Decision_nodeCount(DecisionTreeNode* node) {
     if (!node) return 0;
-
     return 1 + Decision_nodeCount(node->left) + Decision_nodeCount(node->right);
 }
 
-void DecisionTree_destroy(DecisionTreeNode* decisionTree) {
-    if (!decisionTree) return;
+/**
+ * @brief DecisionTree_destroy destroys a decision tree
+ * @param decisionTree the decision tree
+ */
+void DecisionTree_destroy(DecisionTreeNode *decisionTree) {
+    if(!decisionTree) return;
 
     DecisionTree_destroy(decisionTree->left);
     DecisionTree_destroy(decisionTree->right);
 
-    decisionTree->left = NULL;
-    decisionTree->right = NULL;
     free(decisionTree);
 }
--- src/DecisionTree.h
+++ src/DecisionTree.h
@@ -1,7 +1,3 @@
-//
-// Created by loe on 14/12/23.
-//
-
 #ifndef DECISIONTREE_H
 #define DECISIONTREE_H
 #include "Split.h"
@@ -13,8 +9,28 @@ typedef struct sDecisionTreeNode{
     int classID;
 } DecisionTreeNode;
 
+/**
+ * @brief DecisionTree_create creates a decision tree from a subproblem
+ * @param sp the subproblem
+ * @param currentDepth the current depth of the tree
+ * @param maxDepth the maximum depth of the tree
+ * @param prunningThreshold the prunning treshold
+ * @return a pointer to the decision tree
+ */
 DecisionTreeNode* DecisionTree_create(Subproblem* sp, int currentDepth, int maxDepth, float prunningThreshold);
-void DecisionTree_destroy(DecisionTreeNode *decisionTree);
+
+/**
+ * @brief Decision_nodeCount counts the number of nodes in a decision tree
+ * @param node the node
+ * @return the number of nodes
+ */
 int Decision_nodeCount(DecisionTreeNode* node);
+
+/**
+ * @brief DecisionTree_destroy destroys a decision tree
+ * @param decisionTree the decision tree
+ */
+void DecisionTree_destroy(DecisionTreeNode *decisionTree);
+
 
 #endif //DECISIONTREE_H
