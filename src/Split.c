c228030711ca73f03d3efe9517c5343bd5245f24
shaft <sh4ft.me@gmail.com>
2023-12-17 11:55:15 +0100

    feat(split): refacto split function, fixed various issues.

--- src/Split.c
+++ src/Split.c
@@ -1,83 +1,81 @@
-//
-// Created by loe on 14/12/23.
-//
-
 #include "Split.h"
-#include <limits.h>
 
-float Split_impurity(Subproblem *sp) {
-
-    // Handle division by zero
-    if (sp->instanceCount == 0) return 0; // !TODO
-
-    float impurity = 1.0;
-    float temp;
-    for (int i = 0; i < sp->classCount; ++i) {
-        temp = (float)sp->classes[i].instanceCount  / (float)sp->instanceCount;
-        impurity -= temp * temp;
-    }
-
-    return impurity;
-}
-
-float Split_threshold(Subproblem *sp, int featureID) {
-    int minj = INT_MAX, maxj = INT_MIN, value;
-
+static void split_subproblem(Subproblem *sp, int featureID, float threshold, Subproblem *spl, Subproblem *spr) {
     for (int i = 0; i < sp->instanceCount; ++i) {
-        value = sp->instances[i]->values[featureID];
-        if (value < minj) minj = value;
-        if (value > maxj) maxj = value;
-    }
-
-    return (float)(minj + maxj) / 2.0;
-}
-
-float Split_gini(Subproblem *sp, int featureID, float threshold) {
-    Subproblem *spl = Subproblem_create(1, sp->featureCount, sp->classCount);
-    Subproblem *spr = Subproblem_create(1, sp->featureCount, sp->classCount);
-
-    // Split sp into 2 sub problems
-    for (int i = 0; i < sp->instanceCount; ++i) {
-        int value = sp->instances[i]->values[featureID];
-        if ((float)value <= threshold) {
-            Subproblem_insert(spl, sp->instances[i]);
-        }
-        else {
+        (float)sp->instances[i]->values[featureID] <= threshold ?
+            Subproblem_insert(spl, sp->instances[i]) :
             Subproblem_insert(spr, sp->instances[i]);
-        }
     }
-
-
-    // Handle division by zero
-    if (!sp->instanceCount || !((float)spr->instanceCount/(float)sp->instanceCount) * Split_impurity(spr)) {
-        return 1;
-    }
-
-    float gini = ((float)spl->instanceCount/(float)sp->instanceCount) * Split_impurity(spl) +
-                 ((float)spr->instanceCount/(float)sp->instanceCount) * Split_impurity(spr);
-
-
-    // Clamp the result between 0 and 1
-    if (gini > 1) return 1;
-    if (gini < 0) return 0;
-    return gini;
 }
 
-Split Split_compute(Subproblem *subproblem) {
-    Split *split = (Split*) calloc(1, sizeof(Split));
-    split->featureID = -1;
-    float minGini = INT_MAX;
+static float split_compute_impurity(Subproblem *sp) {
+    if (sp->instanceCount == 0) return 0.0;  // Perfectly pure subproblem
 
-    // Test each feature and get the minimum split
-    for (int i = 0; i < subproblem->featureCount; ++i) {
-        float threshold = Split_threshold(subproblem, i);
-        float gini = Split_gini(subproblem, i, threshold);
-        if (gini <= minGini) {
-            minGini = gini;
-            split->threshold = threshold;
-            split->featureID = i;
+    float g = 1.0;
+    for(int classID=0; classID < sp->classCount; ++classID) {
+        const float p = (float)sp->classes[classID].instanceCount / (float)sp->instanceCount;
+        g -= p * p;
+    }
+    return g;
+}
+
+/**
+ * @brief Split_gini computes the gini impurity of a subproblem given a feature and a threshold
+ * @param sp the subproblem
+ * @param featureID the feature to split on
+ * @param threshold the threshold to split on
+ * @return the gini impurity
+*/
+float Split_gini(Subproblem *sp, int featureID, float threshold) {
+    if(sp->instanceCount==0) return 0.0; // Perfectly pure subproblem
+
+    Subproblem *spl = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+    Subproblem *spr = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+
+    split_subproblem(sp, featureID, threshold, spl, spr);
+
+    /* |spl| + |spr| = |sp| => (|spl|+|spr|)/|sp| = 1 => (|spl|/|sp|) + (|spr|/|sp|) = 1
+    * With 0 <= g(spl) <= 1 and 0 <= g(spr) <= 1, we have 0 <= (|spl|/|sp|)*g(spl) + (|spr|/|sp|)*g(spr) <= 1
+    * So 0 <= gini <= 1
+    */
+
+    return (float)spl->instanceCount/(float)sp->instanceCount * split_compute_impurity(spl) +
+           (float)spr->instanceCount/(float)sp->instanceCount * split_compute_impurity(spr);
+}
+
+/**
+ * @brief Split_threshold computes the threshold to use for a given feature based on (minFeatureValue + maxFeatureValue) / 2
+ * @param sp the subproblem
+ * @param featureID the feature to split on
+ * @return the threshold
+ */
+float Split_threshold(Subproblem *sp, int featureID) {
+    int min = 255, max = 0;
+    for(int instanceID=0; instanceID < sp->instanceCount; instanceID++) {
+        const int value = sp->instances[instanceID]->values[featureID];
+        if(value < min) min = value;
+        if(value > max) max = value;
+    }
+    return (float)(min + max) / 2.0;
+}
+
+/**
+ * @brief Split_compute computes the best split for a subproblem
+ * @param sp the subproblem
+ * @return the split
+ */
+Split Split_compute(Subproblem *sp) {
+    Split split = {0, 0.0};
+    float impurity = 1.0;
+
+    for(int featureID=0; featureID < sp->featureCount; featureID++) {
+        const float _threshold = Split_threshold(sp, featureID);
+        const float _impurity = Split_gini(sp, featureID, _threshold);
+        if(_impurity <= impurity) {
+            split.featureID = featureID; split.threshold = _threshold;
+            impurity = _impurity;
         }
     }
 
-    return *split;
+    return split;
 }
--- src/Split.h
+++ src/Split.h
@@ -1,9 +1,6 @@
-//
-// Created by loe on 14/12/23.
-//
-
 #ifndef SPLIT_H
 #define SPLIT_H
+
 #include "Dataset.h"
 
 typedef struct sSplit{
@@ -11,8 +8,27 @@ typedef struct sSplit{
     float threshold;
 } Split;
 
+/**
+ * @brief Split_gini computes the gini impurity of a subproblem given a feature and a threshold
+ * @param sp the subproblem
+ * @param featureID the feature to split on
+ * @param threshold the threshold to split on
+*/
 float Split_gini(Subproblem *sp, int featureID, float threshold);
+
+/**
+ * @brief Split_threshold computes the threshold to use for a given feature based on (minFeatureValue + maxFeatureValue) / 2
+ * @param sp the subproblem
+ * @param featureID the feature to split on
+ * @return the threshold
+ */
 float Split_threshold(Subproblem *sp, int featureID);
-Split Split_compute(Subproblem *subproblem);
+
+/**
+ * @brief Split_compute computes the best split for a subproblem
+ * @param sp the subproblem
+ * @return the split
+ */
+Split Split_compute(Subproblem *sp);
 
 #endif //SPLIT_H
