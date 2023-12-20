4c9d2821c4308d02086b554e836bd767c740b064
Loe159 <loadu44@gmail.com>
2023-12-20 13:27:25 +0100

    Update docstrings

--- src/UserInterface.c
+++ src/UserInterface.c
@@ -10,8 +10,8 @@ int max(int a, int b) {
 }
 
 /**
- * \brief Initialize "SDL" and "SDL image" libraries
- * \return Exit code
+ * @brief Initialize "SDL", "SDL image" and "SDL TTF" libraries
+ * @return Exit code
  */
 int init_sdl() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -37,15 +37,15 @@ int init_sdl() {
 }
 
 /**
- * \brief Draw a pixel on a texture given colour and coordinates
- * \param texture The texture to draw on
- * \param r the red component
- * \param g the greend component
- * \param b the blue component
- * \param a the alpha component
- * \param x the x coordinate of the pixel
- * \param y the y coordinate of the pixel
- * \param overwrite true whether the new pixel be overwritten or added
+ * @brief Draw a pixel on a texture given colour and coordinates
+ * @param texture The texture to draw on
+ * @param r the red component
+ * @param g the greend component
+ * @param b the blue component
+ * @param a the alpha component
+ * @param x the x coordinate of the pixel
+ * @param y the y coordinate of the pixel
+ * @param overwrite true whether the new pixel be overwritten or added
  */
 void draw_pixel(SDL_Texture *texture, Uint8 r, Uint8 g, Uint8 b, Uint8 a, int x, int y, bool overwrite) {
     void* tmp;
@@ -80,10 +80,10 @@ void draw_pixel(SDL_Texture *texture, Uint8 r, Uint8 g, Uint8 b, Uint8 a, int x,
 }
 
 /**
- * \brief load saved drawing from file and predict its class from a given tree.
- * \param tree the tree
- * \param args the arguments for the predict_from_tree function
- * \return the digit predicted
+ * @brief load saved drawing from file and predict its class from a given tree.
+ * @param tree the tree
+ * @param args the arguments for the predict_from_tree function
+ * @return the digit predicted
  */
 int predict_drawing(DecisionTreeNode *tree, PredictFromTreeArgs *args) {
     char tmp_path[128] = "../datasets/test.txt";
@@ -101,11 +101,11 @@ int predict_drawing(DecisionTreeNode *tree, PredictFromTreeArgs *args) {
 }
 
 /**
- * \brief Save a texture into a dataset of one instance
- * \param filename the filename of the output file
- * \param texture the SDL image to export
- * \param window the SDL window
- * \return int the digit predicted
+ * @brief Save a texture into a dataset of one instance
+ * @param filename the filename of the output file
+ * @param texture the SDL image to export
+ * @param window the SDL window
+ * @return int the digit predicted
  */
 int save_texture(const char* filename, SDL_Texture* texture, SDL_Window *window, DecisionTreeNode *tree, PredictFromTreeArgs *args) {
     void* tmp;
@@ -144,11 +144,11 @@ int save_texture(const char* filename, SDL_Texture* texture, SDL_Window *window,
 
 
 /**
- * \brief Load a dataset and display it on the texture
- * \param filename the filename of the output file
- * \param texture the SDL image to export
- * \param window the SDL window
- * \return void
+ * @brief Load a dataset and display it on the texture
+ * @param filename the filename of the output file
+ * @param texture the SDL image to export
+ * @param window the SDL window
+ * @return void
  */
 void load_texture(const char* filename, SDL_Texture* texture, SDL_Window *window) {
     Dataset *trainData = parse_dataset_from_file(filename);
@@ -164,11 +164,11 @@ void load_texture(const char* filename, SDL_Texture* texture, SDL_Window *window
 
 
 /**
- * \brief Draw a plein circle on a given position colorized gradiently from white to black
- * \param texture The SDL texture to draw on
- * \param x The x coordinate
- * \param y The y coordinate
- * \param radius The radius of the circle
+ * @brief Draw a plein circle on a given position colorized gradiently from white to black
+ * @param texture The SDL texture to draw on
+ * @param x The x coordinate
+ * @param y The y coordinate
+ * @param radius The radius of the circle
  */
 void draw_circle(SDL_Texture *texture, int x, int y, float radius, bool subtract) {
     // Loop in each pixels in the radius
@@ -193,8 +193,8 @@ void draw_circle(SDL_Texture *texture, int x, int y, float radius, bool subtract
 }
 
 /**
- * \brief Entirely colorize texture to black
- * \param texture The SDL texture to draw on
+ * @brief Entirely colorize texture to black
+ * @param texture The SDL texture to draw on
  */
 void reset_drawing(SDL_Texture *texture) {
     for (int i = 0; i < TEXTURE_WIDTH; ++i) {
