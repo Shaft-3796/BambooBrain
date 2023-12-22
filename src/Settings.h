e007146b12c5b6298dbd2be0489a7731c5e44091
x <sh4ft.me@gmail.com>
2023-12-22 23:30:52 +0100

    feat: formatted for VPL

--- src/Settings.h
+++ src/Settings.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define VERBOSE 1  // whether to show verbose information
+#define VERBOSE 0  // whether to show verbose information
 
 #if VERBOSE == 1
 #define bbprintf(...) printf(__VA_ARGS__)
